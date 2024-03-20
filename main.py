369d7a069eb78aafe7862069ea2c3b46c612180c
Conner <connerj.layne@gmail.com>
2024-03-20 08:40:19 -0400

    added comments with names

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#Yazan Al-Hawamdeh
 def encode(password):
     return ''.join(str((int(digit) + 3) % 10) for digit in password)
 
@@ -20,6 +21,7 @@ def main():
                 print("Invalid input. Please ensure your password is 8 digits long and only contains numbers.")
 
         elif choice == '2':
+            #Conner Layne
             print(f'The encoded password is {stored_encoded_password}, and the original password is {decode(stored_encoded_password)}.')
 
         elif choice == '3':
